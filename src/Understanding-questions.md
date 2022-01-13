3819b8d9f7439716f35b4289fafedabc56f483b9
Allison Stewart <amstewart@yahoo.com>
2022-01-13 17:29:29 -0500

    completed first 3 tests

---
+++ src/Understanding-questions.md
@@ -0,0 +1,13 @@
+1. click on first name input
+2. type in first name input
+3. error message appears if name is not a minimum of 5 characters
+4. click in last name input
+5. type in last name input
+6. error message appears if no last name is entered
+7. click in email input
+8. type in email input
+9. see error message if user does not type a valid email
+10. click in message input
+11. type in message input
+12. click on submit button
+13. box appears above submit button with a summary of what user submitted (first, last, email, message)
--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -6,15 +6,21 @@ import userEvent from '@testing-library/user-event';
 import ContactForm from './ContactForm';
 
 test('renders without errors', ()=>{
-    
+    render(<ContactForm/>);
 });
 
 test('renders the contact form header', ()=> {
-    
+    render(<ContactForm/>);
+    const header = screen.queryByText(/contact form/i);
+    expect(header).toBeInTheDocument();
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
-    
+    render(<ContactForm/>);
+    const input = 'John'
+
+    const firstname = screen.getByPlaceholderText('Edd');
+    userEvent.type(firstname, input)
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
